32c577edbf71b71ff0c57c2679d329eb8c345669
arleut <lucass.vsbb@gmail.com>
2026-04-15 00:05:35 +0000

    First Sprite (Blue Cube)!

---
+++ main.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas. width = 800;
+canvas.height = 600;
+ctx.fillStyle = 'blue';
+ctx.fillRect(100, 100, 100, 100);
